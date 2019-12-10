1a7086afb29688d4d872d7f9e6f9a8dd10bebc36
Dean Shub <deanshub@gmail.com>
2019-12-10 18:13:24 +0200

    just logging

--- scripts/post-merge.ts
+++ scripts/post-merge.ts
@@ -1,7 +1,5 @@
 import execa from 'execa';
 import chalk from 'chalk';
-import prompts from 'prompts';
-import hasYarn from 'has-yarn';
 
 (async () => {
   const args = [
@@ -16,20 +14,10 @@ import hasYarn from 'has-yarn';
   const gitDiff = execa.sync('git', args);
 
   if (/package\.json|yarn\.lock/.test(gitDiff.stdout)) {
-    const response: { install: string } = await prompts({
-      type: 'text',
-      name: 'install',
-      message: chalk.green(
-        'Some dependencies changed, should I run `npm install`? [y/N]',
+    console.log(
+      chalk.yellow(
+        'Seems like some dependencies changed, consider running `yarn`',
       ),
-    });
-    const install = response.install.toLowerCase();
-    if (install === 'y' || install === 'yes') {
-      if (hasYarn()) {
-        execa.sync('yarn', { stdio: 'inherit' });
-      } else {
-        execa.sync('npm', ['install'], { stdio: 'inherit' });
-      }
-    }
+    );
   }
 })();
